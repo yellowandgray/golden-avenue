b00c2cdc116e41b1663a6385adc9e9a956d73573
Y&G <Y&G@192.168.0.11>
2018-12-18 12:58:34 +0530

    style sheet changed

--- it-security.php
+++ it-security.php
@@ -4,7 +4,7 @@
     <body>
         <div id="wrapper">
             <?php include 'menu.php'; ?><!-- end header -->
-            <section id="inner-headline">
+            <section id="inner-headline-3">
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-12">
