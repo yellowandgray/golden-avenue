82afbd199bec6b7df4753ab438144f76dce9e328
proje <proje@192.168.0.7>
2018-12-28 13:57:24 +0530

    common and onload popup pages updated

--- onload-popup.php
+++ onload-popup.php
@@ -6,7 +6,7 @@
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <img  class="img-width" src="img/sub-banner/onload-pic-1.jpg" alt=""/>
                 <h3 class="text-center margin-top-10">Join us for a live demo</h3>
-                <span class="text-center">We will walk you through the product suite and answer any questions you have about FirstBIT ERP</span>
+                <p class="text-center line-height-25">We will walk you through the product suite and answer any questions you have about FirstBIT ERP</p>
 
             </div>
             <div class="modal-body-1">
