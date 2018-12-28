bb8dd403361d0838735bd4c7cf1bc623dfca27e5
proje <proje@192.168.0.7>
2018-12-28 16:39:15 +0530

    chat popup and common css updated

--- chat-popup.php
+++ chat-popup.php
@@ -4,7 +4,7 @@
     <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="closePopup()"><span aria-hidden="true">×</span></button>
     <form action="/action_page.php" class="chat-popup-form-container">
         <h1>Chat</h1>
-        <textarea placeholder="Type message.." name="msg" required></textarea>
+        <div class="textarea-1"></div>
         <input type="text" class="chat-input" placeholder="Type Your Message" style="float: left;"/><button type="button" class="btn" style="float: right;">Send</button>
     </form>
 </div>
