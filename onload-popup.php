580fe372255b5cab62e8f34da0678e04580020b8
Y&G <Y&G@192.168.0.11>
2018-12-28 13:34:18 +0530

    onload popup added

---
+++ onload-popup.php
@@ -0,0 +1,25 @@
+  <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-onload-content">
+                    <div class="modal-header-1">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <img  class="img-width" src="img/sub-banner/onload-pic-1.jpg" alt=""/>
+                        <h3 class="text-center margin-top-10">Join us for a live demo</h3>
+                        
+                    </div>
+                    <div class="modal-body-1">
+                        <form action="" method="post" id="footer-form">
+                            <h3 class="text-center bold margin-bottom-10">Enquiry</h3>
+                            <input class="form-control" name="name" id="first_name" placeholder="Name *" required>
+                            <input class="form-control" name="email" id="email" placeholder="Your Email *">
+                            <input class="form-control" name="phone" placeholder="Phone *" id="phone">
+                            <center>
+                                <input type="submit" class="btn btn-blue" value="Schedule Demo">
+                            </center>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
