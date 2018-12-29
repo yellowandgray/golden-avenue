f6268d628519ce8becf89083353aa6fae86cba9b
Y&G <Y&G@192.168.0.11>
2018-12-29 19:08:48 +0530

    correction

--- faq-cyber-security.php
+++ faq-cyber-security.php
@@ -11,7 +11,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-12">
-                            <h2 class="pageTitle">CYBER SECURITY AWARENESS</h2>
+                            <h2 class="pageTitle">CYBER SECURITY AWARENESS<br/>FAQ</h2>
                         </div>
                     </div>
                 </div>
