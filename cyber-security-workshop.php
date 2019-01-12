a73bbc1dc0eab3299a39b9c53a5669ff2bb4c87e
proje <proje@192.168.0.7>
2019-01-12 15:50:27 +0530

    cyber security workshop changed

--- cyber-security-workshop.php
+++ cyber-security-workshop.php
@@ -1,5 +1,5 @@
 <div class="cyber-security-bg">
-    <a href="cyber-security.php"><h4 class="text-center bold"> AED 1 CYBER SECURITY AWARENESS WORKSHOP</h4></a>
+    <a href="cyber-security.php"><h4 class="text-center bold">Cyber Security Awareness Workshop at your Premise for AED 1</h4></a>
     <a href="cyber-security.php"><img src="img/golden-cyber.jpg" class="img-responsive img-width" /></a>
     <p>- Protect your business <br/>- Make employees your active defense against cyber threats </p>
     <hr class="hr1"> 
