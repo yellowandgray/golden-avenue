918b4299c55e6874afdcda18f4ff8b40365b7fce
proje <proje@192.168.0.7>
2019-01-12 15:37:09 +0530

    cyber security workshop changed

--- cyber-security-workshop.php
+++ cyber-security-workshop.php
@@ -3,6 +3,6 @@
     <a href="cyber-security.php"><img src="img/golden-cyber.jpg" class="img-responsive img-width" /></a>
     <p>- Protect your business <br/>- Make employees your active defense against cyber threats </p>
     <hr class="hr1"> 
-    <p class="font-size-12 bold">Venue:&nbsp;&nbsp;Your Office Premise <a href="cyber-security.php" type="buttton" class="btn-custom">See More</a></p>
+    <a href="cyber-security.php" type="buttton" class="btn-custom">See More</a>
 <!--                                        <p class="font-size-12 bold">Date: &nbsp;&nbsp;&nbsp; January 01, 2019</p>-->
 </div>
