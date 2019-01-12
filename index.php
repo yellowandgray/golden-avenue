6d1ce35a4d9edb9b5c05f69abc08ae3971b026a8
proje <proje@192.168.0.7>
2019-01-12 13:43:19 +0530

    all pages done

--- index.php
+++ index.php
@@ -110,11 +110,11 @@
                         </div>
                         <div class="col-md-5 wow fadeInUp">
                             <div class="cyber-security-bg1">
-                                <a href="faq-cyber-security.php"><h4 class="text-center bold">Cyber Security Awareness Workshop at your Premise for AED 1</h4></a>
-                                <a href="faq-cyber-security.php"><img src="img/golden-cyber.jpg" class="img-responsive img-width" />
+                                <a href="cyber-security.php"><h4 class="text-center bold">Cyber Security Awareness Workshop at your Premise for AED 1</h4></a>
+                                <a href="cyber-security.php"><img src="img/golden-cyber.jpg" class="img-responsive img-width" /></a>
                                     <p>- Protect your business <br/>- Make employees your active defense against cyber threats </p>
                                     <hr class="hr1"> 
-                                    <p class="font-size-12 bold">Venue: Your Office Premise <a href="cyber-security.php" type="buttton" class="btn-custom">See More</a></p>
+                                    <a href="cyber-security.php" type="buttton" class="btn-custom">See More</a>
     <!--                                        <p class="font-size-12 bold">Date: &nbsp;&nbsp;&nbsp; January 01, 2019</p>-->
                             </div>
                         </div>
