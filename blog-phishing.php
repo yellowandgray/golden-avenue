aa91458ffe2796b6674b1eee539d08f3fac5aad9
Y&G <Y&G@192.168.0.11>
2019-04-02 16:10:57 +0530

    CONTACT FORM ADDED

--- blog-phishing.php
+++ blog-phishing.php
@@ -73,8 +73,8 @@
                                 <li> Be aware that Banks never ask for confidential information via emails, calls or texts.</li>
                                 <li> Whenever you are sharing your personal or financial information online, ensure it’s secure. Make sure that the website’s URL begins with “https” instead of “http”, and has a lock symbol on the website. Clicking on the lock icon should display the digital certificate of authenticity of the website.</li>
                             </ul>
-                            <p>For more information on how to protect yourself and your employees from these cyber crimes,</p>
-                            <p class="text-justify">Give us a Call <a href="tel:+04 2668272">04 2668272</a> for a No Obligation Free Consultation</p>
+                            <p class="text-justify">For more information on how to protect yourself and your employees from these cyber crimes give us a Call <a href="tel:+04 2668272">04 2668272</a> for a No Obligation Free Consultation</p>
+                            
                         </div>
                         <!--                        <div class="col-md-5 wow fadeInRight">
                        
