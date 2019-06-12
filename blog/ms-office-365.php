593f2255fdd4168addf2a010b74a9889220f8071
Y&G <Y&G@192.168.1.4>
2019-06-12 15:28:16 +0530

    blog pages updated

---
+++ blog/ms-office-365.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php $page = 'blog'; ?>
+        <meta charset="utf-8">
+        <title>Golden Avenue | Your Virtual IT team</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Data Protection: Why it matters? Protection of your data from threats posed by Hackers and Malware, Failed hardware, natural calamity or unauthorized access to the network etc., needs a good Annual Maintenance Contract Provider" />
+        <meta name="keywords" content="Annual Maintenance Contract, Dubai, Data Protection, Annual Maintenance Contract Provider, AMC, AMC Dubai, Annual Maintenance Contract, cybercrimes, IT Infrastructure, IT & Network Health Check, Consultation">
+        <meta name="author" content="" />
+        <link rel="icon" href="../img/favicon.png" type="image">
+        <!-- css -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet" />
+        <link href="../css/fancybox/jquery.fancybox.css" rel="stylesheet"> 
+        <link href="../css/flexslider.css" rel="stylesheet" /> 
+        <link href="../css/style.css" rel="stylesheet" />
+        <link href="../css/common.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC:100,300,400,500,700,900&amp;subset=chinese-traditional,japanese" rel="stylesheet">
+        <link href="../css/libs/animate.css" rel="stylesheet" type="text/css"/>
+        <!--    <link href="css/site.css" rel="stylesheet" type="text/css"/>-->
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+              <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+            <![endif]-->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137857896-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-137857896-1');
+        </script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <?php include 'menu.php'; ?>
+            <!-- end header -->
+            <section id="inner-headline-9" class="wow fadeInUp">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h2 class="pageTitle"></h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="content">
+                <div class="container">
+                    <ul class="breadcrumb">
+                        <li><a href="../index.php">Home</a></li>
+                        <li><a href="../blog.php">Blog</a></li>
+                        <li>Office 365</li>
+                    </ul>
+                    <div class="row showcase-section">
+                        <div class="col-md-12 wow fadeInLeft">
+                            <h2 class="font-size-42">Office 365: 7 Reasons to increase your Business Productivity</h2>
+                            <p>Posted by: <span class="color-blue">Golden Avenue</span> | Date: <span class="color-blue">17 May 2019</span></p>
+                            <img src="../img/blog/office365.jpg" alt="Blog" class="img-responsive"/>
+                            <br/>
+                            <h4><b>What is Office 365?</b></h4>
+                            <p>Office 365 is a subscription service offered by Microsoft that allows users to download and install Microsoft Office apps to use with both Windows and OS X operating systems. Office 365 is more than just a set of tools but it’s also a new way of working. </p>
+                            <p> It's an excellent choice for small businesses that want easy access to Microsoft’s powerful office productivity suite (Excel, Word, PowerPoint, OneNote, Outlook, etc.) on desktop, browser, and mobile devices and cloud storage but don't want to switch their email to Microsoft servers.</p>
+                            <h4><b>What are the main benefits of Office 365 for SMBs?</b></h4>
+                            <ol style="list-style: none;">
+                                <li><b>1.Ability to work anytime Anywhere</b><br>Working remotely is one of the cool features of Office 365. You don’t need to have any IT skills or the need to configure it. All you need is an internet connection and you can start working. It’s that simple!</li>
+                                <li><b>2.Data storage</b><br>High performance cloud storage via OneDrive is a powerful cloud storage service that offers up an immense amount of cloud storage per user, and stays readily synced across multiple machines or devices and make your files available anywhere. It allows you to easily share files with other people, including those who do not have Office 365. </li>
+                                <li><b>3.Data Security</b><br>Office 365 has built-in security and constant compliance so that small businesses can focus on the more important things. You always know where your data is and the persons who can access it. You can also remotely erase all data if you lose your mobile device. By storing your data in the Cloud, your data is safer than ever before.</li>
+                                <li><b>4.Get Organized </b><br>In a small business, Meetings email and contact management are a big part in day to day operations.  Emails, calendar and contacts are all synchronized to work together when you use Office 365. For example, when you update a contact on your mobile phone and it will be automatically updated across all your devices. This is a useful feature to help small businesses get the most from Office 365. It also allows you to access the same data from any device. If you save a document on your PC, you still can continue working on it on your mobile or tablet.</li>
+                                <li><b>5.Co-authoring </b><br>In addition to the desktop and mobile device clients, with Office 365 you can read and edit documents with your teammates, in real time, via a web browser. This works in union with OneDrive or SharePoint Online.</li>
+                                <li><b>6.Device Freedom</b><br>You can install the desktop or mobile device suite on up to 5 different devices of each type (desktop/pc, phone, or tablet) at a time with various OS. This option is mostly useful for people who use both Windows and Mac devices on a regular basis or for those who rely on multiple computers.</li>
+                                <li><b>7.Flexible Plans to choose from</b><br>A business can pick and choose Office 365 plans for their employees based on their needs. Microsoft offers Office 365 Business, Business Essentials, and Business Premium plans each has a limit of 300 users, while the Enterprise plans are for an unlimited number of users. </li>
+                            </ol>
+                            <br/>
+                            <p class="text-justify">Give us a Call <a href="tel:+04 2668272">04 2668272</a> at <a href=../index.php>Golden Avenue</a> for a No Obligation Free Consultation.</p>
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <?php include 'footer.php'; ?>
+
+        </div>
+
+    </body>
+</html>
